2321d640aa99c965c53321d849ef3da33c5cfaad
Arindam-Das <arindam123das456@gmail.com>
2018-02-17 14:25:16 +0530

    Added Function interface

---
+++ src/main/java/arundel/util/Function.java
@@ -0,0 +1,32 @@
+/**
+ * Interface for abstracting the concept of mathematical functions.
+
+ * A function  essentiallly operates on a set of inputs and returns an output.
+ * It also has a property called derivative which denotes its rate of change with
+ * the change in one of its parameters. Also, for implementation purposes we also
+ * specify the number of inputs that a {@link Function} needs to operate.
+ */
+
+ public interface Function {
+
+     /**
+      * Returns the no of inputs that this function needs.
+      * @return the number of inputs required.
+      */
+     int noOfInputs();
+
+     /**
+      * Operates on the given set of inputs and returns the result of
+      * the operation. This is the elixir of the concept of functions.
+      * @param parameters to be operated on
+      * @return the result of the operation
+      */
+     double operate(double[] parameters);
+
+     /**
+      * Returns the partial derivatives of this function with
+      * respect to its parameters as an array of {@link Function}
+      * @return array of partial derivative {@link Function}s
+      */
+     Function[] derivatives();
+ }
